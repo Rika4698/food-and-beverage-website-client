57e9529d3416dcf4ddf3a5cb5b550426585d65d3
Rika4698 <rikasharmin181@gmail.com>
2023-10-19 12:36:50 +0600

    create banner in home page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,12 +4,12 @@ const Banner = () => {
     return (
         <div>
          <div className="hero h-[500px]" style={{backgroundImage: 'url(https://i.ibb.co/GdcjhrC/wepik-export-20231018153418-Hu-Tl.png)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
+  <div className="hero-overlay bg-opacity-50"></div>
+  <div className="hero-content text-center  text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="mb-5 text-6xl font-bold text-center text-emerald-400">Best Food And Beverages Industry of BD</h1>
+      <p className="mb-5 text-lg text-sky-400">Here you can get organic and fresh food in your healthy life.</p>
+      <button className="btn bg-sky-500">Shop Now</button>
     </div>
   </div>
 </div>
